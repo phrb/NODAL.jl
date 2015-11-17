7fb0b77edb73f53affa6554cee52cb7384a79a0e
phrb <pedro.bruel@gmail.com>
2015-11-17 11:17:17 -0200

    Change 'Result' constructor syntax.

--- src/core/measurement/results.jl
+++ src/core/measurement/results.jl
@@ -11,20 +11,15 @@ type Result{T <: Configuration, R <: Number} <: AbstractResult
     is_final::Bool
     current_time::Float64
 
-    function Result(technique::ASCIIString, start::T, minimum::T,
-                    cost_minimum::R, iterations::Int, current_iteration::Int,
-                    cost_calls::Int, is_final::Bool)
-        new(technique, start, minimum, cost_minimum, iterations,
-            current_iteration, cost_calls, is_final)
+    function call{T <: Configuration, R <: Number}(::Type{Result},
+                                                   technique::ASCIIString,
+                                                   start::T,
+                                                   minimum::T,
+                                                   cost_minimum::R,
+                                                   iterations::Int,
+                                                   current_iteration::Int,
+                                                   cost_calls::Int, is_final::Bool)
+        new{T, R}(technique, start, minimum, cost_minimum,
+                  iterations, current_iteration, cost_calls, is_final)
     end
-end
-
-function Result{T <: Configuration, R<:Number}(technique::ASCIIString,
-                                               start::T, minimum::T,
-                                               cost_minimum::R,
-                                               iterations::Int,
-                                               current_iteration::Int,
-                                               cost_calls::Int, is_final::Bool)
-    Result{T, R}(technique, start, minimum, cost_minimum, iterations,
-                 current_iteration, cost_calls, is_final)
 end
