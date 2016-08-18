93ffd9babf182a65ac9b531af6d92a88638d53e5
Pedro Bruel <pedro.bruel@gmail.com>
2016-08-17 23:13:19 -0300

    Add uniform search task initialization.
    All search tasks start at the default configuration.

--- src/core/search/tools/initialize_search_tasks!.jl
+++ src/core/search/tools/initialize_search_tasks!.jl
@@ -4,24 +4,21 @@ function initialize_search_tasks!(tuning_run::Run)
     instance_id    = 1
     results        = RemoteRef[]
 
+    tuning_run.cost_values    = zeros(tuning_run.cost_evaluations)
+    tuning_run.starting_cost  = tuning_run.measurement_method(tuning_run,
+                                                              tuning_run.starting_point)
+    initial_result            = Result("Initialize",
+                                       tuning_run.starting_point,
+                                       tuning_run.starting_point,
+                                       tuning_run.starting_cost,
+                                       1, 1, 1, false)
+
     for i = 1:size(tuning_run.methods, 1)
-                tuning_run.cost_values    = zeros(tuning_run.cost_evaluations)
-
-                tuning_run.starting_point = perturb!(tuning_run.starting_point)
-                tuning_run.starting_cost  = tuning_run.measurement_method(tuning_run,
-                                                                          tuning_run.starting_point)
-
-                initial_result            = Result("Initialize",
-                                                   tuning_run.starting_point,
-                                                   tuning_run.starting_point,
-                                                   tuning_run.starting_cost,
-                                                   1, 1, 1, false)
-
         for j = 1:tuning_run.methods[i, 2]
-            worker                    = consume(next_proc)
-            push!(results, RemoteRef(() -> ResultChannel(initial_result), worker))
+            worker = consume(next_proc)
+            push!(results, RemoteRef(() -> ResultChannel(deepcopy(initial_result)), worker))
 
-            reference                 = results[instance_id]
+            reference = results[instance_id]
             remotecall(worker, eval(tuning_run.methods[i, 1]),
                        deepcopy(tuning_run), reference)
             instance_id += 1
