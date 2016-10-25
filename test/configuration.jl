3cb504e23af8105f8962fe511a9b540c5a54c6e3
phrb <pedro.bruel@gmail.com>
2016-10-25 18:53:57 -0200

    Move configuration tests to Base.Test.

--- test/configuration.jl
+++ test/configuration.jl
@@ -1,27 +1,27 @@
-using StochasticSearch, FactCheck
+using StochasticSearch, Base.Test
 
-facts("[Configuration]") do
-    context("constructors") do
+@testset "Configuration" begin
+    @testset "constructors" begin
         c = Configuration("test")
         p = [StringParameter("value",  "test1"),
              IntegerParameter(1, 5, 4, "test2")]
         c = Configuration(p, "test")
-        @fact (c["test1"] == p[1])                   --> true
-        @fact (c["test2"] == p[2])                   --> true
+        @test c["test1"] == p[1]
+        @test c["test2"] == p[2]
         p = Dict{String, Parameter}()
         p["test1"] = StringParameter("value",  "test")
         p["test2"] = IntegerParameter(1, 5, 4, "test")
         c = Configuration(p, "test")
-        @fact (c["test1"] == p["test1"])             --> true
-        @fact (c["test2"] == p["test2"])             --> true
+        @test c["test1"] == p["test1"]
+        @test c["test2"] == p["test2"]
         p = [StringParameter("valuea",  "test1"),
              StringParameter("valueb", "test2"),
              IntegerParameter(1, 44, 3, "test3")]
         c = Configuration(p, "test")
-        @fact (c["test1"] == p[1])                   --> true
-        @fact (c["test2"] == p[2])                   --> true
+        @test c["test1"] == p[1]
+        @test c["test2"] == p[2]
     end
-    context("perturb!") do
+    @testset "perturb!" begin
         p = [StringParameter("valuea",  "test1"),
              StringParameter("valueb", "test2"),
              IntegerParameter(1, 44, 3, "test3")]
@@ -36,64 +36,64 @@ facts("[Configuration]") do
         r = perturb!(c)
         r = perturb!(c)
         r = perturb!(c)
-        @fact (typeof(r["test1"]) <: Configuration)              --> true
-        @fact (r["test1"]["test1"].value == "valuea")            --> true
-        @fact (r["test1"]["test2"].value == "valueb")            --> true
-        @fact (r["i2"].value             == "value")             --> true
-        @fact (typeof(r["test1"]["test3"]) <: NumberParameter)   --> true
-        @fact (typeof(r["i0"])             <: NumberParameter)   --> true
-        @fact (typeof(r["i1"])             <: NumberParameter)   --> true
-        @fact (typeof(r["i3"])             <: NumberParameter)   --> true
+        @test typeof(r["test1"]) <: Configuration
+        @test r["test1"]["test1"].value == "valuea"
+        @test r["test1"]["test2"].value == "valueb"
+        @test r["i2"].value             == "value"
+        @test typeof(r["test1"]["test3"]) <: NumberParameter
+        @test typeof(r["i0"])             <: NumberParameter
+        @test typeof(r["i1"])             <: NumberParameter
+        @test typeof(r["i3"])             <: NumberParameter
         d = Dict{String, Any}()
-        d["i0"]             = 3
-        d["i1"]             = 4
-        d["i3"]             = 2.3
-        d["test1"]          = Dict{String, Any}()
-        d["test1"]["test3"] = 2
-        @fact (typeof(perturb!(c, d)) <: Configuration)          --> true
-        r = perturb!(c, d)
-        r = perturb!(c, d)
-        r = perturb!(c, d)
-        r = perturb!(c, d)
-        @fact (r["i0"].value == c["i0"].value)                   --> true
-        @fact (r["i1"].value == c["i1"].value)                   --> true
-        @fact (r["i3"].value == c["i3"].value)                   --> true
-        @fact (r["test1"]["test3"] == c["test1"]["test3"])       --> true
-        d["test1"]["test1"] = 23
-        @fact_throws MethodError perturb!(c, d)
+        d["i0"]                    = 3
+        d["i1"]                    = 4
+        d["i3"]                    = 2.3
+        d["test1"]                 = Dict{String, Any}()
+        d["test1"]["test3"]        = 2
+        @test (typeof(perturb!(c, d)) <: Configuration)
+        r                          = perturb!(c, d)
+        r                          = perturb!(c, d)
+        r                          = perturb!(c, d)
+        r                          = perturb!(c, d)
+        @test r["i0"].value       == c["i0"].value
+        @test r["i1"].value       == c["i1"].value
+        @test r["i3"].value       == c["i3"].value
+        @test r["test1"]["test3"] == c["test1"]["test3"]
+        d["test1"]["test1"]        = 23
+        @test_throws MethodError perturb!(c, d)
     end
-    context("neighbor!") do
+    @testset "neighbor!" begin
         l = [IntegerParameter(1, 38, 3, "i0"),
              IntegerParameter(4, 66, 55, "i1"),
              StringParameter("value", "i2"),
              FloatParameter(2.33, 85.33, 22.2, "i3")]
-        d = Dict{String, Any}()
-        c = Configuration(l, "test")
+        d        = Dict{String, Any}()
+        c        = Configuration(l, "test")
         println(c)
-        d["i0"] = 5
-        v = c["i0"].value
+        d["i0"]  = 5
+        v        = c["i0"].value
         neighbor!(c, d)
-        @fact v                                              --> not(exactly(c["i0"].value))
-        v = c["i0"].value
+        @test v != c["i0"].value
+        v        = c["i0"].value
         neighbor!(c, d, distance = 9)
-        d = Dict{String, Any}()
-        d["i2"] = 5
-        @fact_throws MethodError neighbor!(c, d)
-        @fact_throws MethodError neighbor!(c, d, 2)
-        l = [IntegerParameter(1, 38, 3, "i0")]
-        c = Configuration(l, "test2")
-        v = c["i0"].value
+        d        = Dict{String, Any}()
+        d["i2"]  = 5
+        @test_throws MethodError neighbor!(c, d)
+        @test_throws MethodError neighbor!(c, d, 2)
+        l        = [IntegerParameter(1, 38, 3, "i0")]
+        c        = Configuration(l, "test2")
+        v        = c["i0"].value
         neighbor!(c)
-        @fact v                                              --> not(exactly(c["i0"].value))
+        @test v != c["i0"].value
     end
-    context("update! and convert") do
-        c     = Configuration("test")
-        c["test"] = IntegerParameter(1, 2323, 2, "i0")
-        a     = convert(Array{Parameter}, c)
-        @fact (a[1] == c["test"])                            --> true
+    @testset "update! and convert" begin
+        c                    = Configuration("test")
+        c["test"]            = IntegerParameter(1, 2323, 2, "i0")
+        a                    = convert(Array{Parameter}, c)
+        @test a[1]          == c["test"]
         push!(a, StringParameter("name", "i2"))
         update!(c, a)
-        @fact (c["i2"] != a[1])                              --> true
-        @fact (c["i2"].value == a[2].value)                  --> true
+        @test c["i2"]       != a[1]
+        @test c["i2"].value == a[2].value
     end
 end
