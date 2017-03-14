8017780b74cac73282fb85303600b2b635764453
phrb <pedro.bruel@gmail.com>
2017-03-14 14:32:52 -0300

    Measurement function uses pmap

--- src/core/measurement/measure.jl
+++ src/core/measurement/measure.jl
@@ -1,26 +1,15 @@
 function measure_mean!(tuning_run::Run, x::Configuration)
-    i = 1
-    next_index() = (index = i; i += 1; index)
+    configurations = Array{Configuration}(tuning_run.cost_evaluations)
+    fill!(configurations, x)
 
-    @sync begin
-        for p = 1:nprocs()
-            if p != myid() || nprocs() == 1
-                @async begin
-                    while true
-                        index = next_index()
-                        if index > tuning_run.cost_evaluations
-                            break
-                        end
-                        tuning_run.cost_values[index] = remotecall_fetch(tuning_run.cost,
-                                                                         p,
-                                                                         x,
-                                                                         tuning_run.cost_arguments)
-                    end
-                end
-            end
-        end
+    pmap_cost(x::Configuration) = tuning_run.cost(x, tuning_run.cost_arguments)
+    results = pmap(pmap_cost, configurations)
+
+    for i = 1:tuning_run.cost_evaluations
+        tuning_run.cost_values[i] = results[i]
     end
-    mean(tuning_run.cost_values)
+
+    mean(results)
 end
 
 function sequential_measure_mean!(tuning_run::Run, x::Configuration)
